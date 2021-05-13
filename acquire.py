09cdb81a69e48e0e74c9a6f5209d9176868ca31e
lupeluna <80718680+lupeluna@users.noreply.github.com>
2021-05-13 18:01:20 -0500

    acquire questions

---
+++ acquire.py
@@ -0,0 +1,30 @@
+# Make a new python module, acquire.py to hold the following data aquisition functions:
+
+
+# 1.  Make a function named get_titanic_data that returns the titanic data from the codeup 
+# data science database as a pandas data frame. Obtain your data from the Codeup Data 
+# Science Database.
+
+
+
+
+
+# 2. Make a function named get_iris_data that returns the data from the iris_db on the 
+# codeup data science database as a pandas data frame. The returned data frame should 
+# include the actual name of the species in addition to the species_ids. Obtain your 
+# data from the Codeup Data Science Database.
+
+
+
+
+
+
+# 3.  Once you've got your get_titanic_data and get_iris_data functions written, now 
+# it's time to add caching to them. To do this, edit the beginning of the function to check 
+# for a local filename like titanic.csv or iris.csv. If they exist, use the .csv file. 
+# If the file doesn't exist, then produce the SQL and pandas necessary to create a dataframe, 
+# then write the dataframe to a .csv file with the appropriate name.
+
+
+
+
